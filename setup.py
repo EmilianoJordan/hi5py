b0842cc556fb33a9ef589ef60ffa5326bd9febfc
Emiliano Jordan <emiliano.jordan+github@protonmail.com>
2021-07-12 18:30:31 -0600

    Setup Tools

---
+++ setup.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+"""The presence of this file ensures the support
+of pip editable mode *with setuptools only*.
+
+Thanks to work done by Sorin Sbarnea on cookiecutter
+https://github.com/cookiecutter/cookiecutter/pull/1577
+"""
+import setuptools
+
+setuptools.setup(
+    use_scm_version={"local_scheme": "no-local-version"},
+    setup_requires=["setuptools_scm[toml]>=3.5.0"],
+)
